70d9df35f89981d1506a08601a35b68018b81980
psippl <philsippl@gmail.com>
2022-04-17 15:00:30 +0200

    up

--- lib/vm/src/trap/traphandlers.rs
+++ lib/vm/src/trap/traphandlers.rs
@@ -221,7 +221,7 @@ cfg_if::cfg_if! {
                     cx.uc_mcontext.pc as *const u8
                 } else if #[cfg(all(target_os = "android", target_arch = "arm"))] {
                     // nop
-                    0 as *const u8;
+                    0 as *const u8
                 } else if #[cfg(all(target_os = "macos", target_arch = "x86_64"))] {
                     let cx = &*(cx as *const libc::ucontext_t);
                     (*cx.uc_mcontext).__ss.__rip as *const u8
